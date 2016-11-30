da70595f17a0c34382586f808f135bdf8ac602ee
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2016-11-30 09:27:47 +0300

    main method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        BomberBase bb = new BomberBase();
+
+        bb.bomb(new int[]{1, 2}, true);
+
+        bb.bomb(new int[]{1, 2}, false);
+
     }
 }
